ff8f690eac5e6aaee433a5682ebf4d1f100380d2
Andrey Kovalenko <ak@taucode.com>
2019-12-21 11:12:26 +0200

    Pushing to nuget.org feed now.

--- nuget-push-z1.cmd
+++ nuget-push-z1.cmd
@@ -1,2 +1,1 @@
-rem pushes one file to the NuGet feed
-nuget.exe push -Source http://nuget.taucode.com -ApiKey 4adb822f9e46446f42dfa06457ee6be6 %1
+dotnet nuget push %1 -k oy2fswonuoa7nsfv5hoa3cvgndpx555urfmvfsbun4rf6m -s https://api.nuget.org/v3/index.json
